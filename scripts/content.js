afbc8fe89e16ae4514c4a50bd285eafcf09066fa
psirotkin <pawel.sirotkin@gmail.com>
2023-03-21 10:52:06 +0100

    Hiding mahine translation hits from matches tab

--- scripts/content.js
+++ scripts/content.js
@@ -5,9 +5,16 @@ observer.observe(document, {
 });
 
 const matchesTab = document.getElementsByClassName('matches');
+const machineTranslationHits = document.getElementsByClassName('suggestion-item');
+// 
 
 function onMutation(mutations) {
   for (var i = 0, tab; (tab = matchesTab[i]); i++) {
     tab.classList.add('open')
+  }
+  for (var i = 0, tmHit; (tmHit = machineTranslationHits[i]); i++) {
+    if (tmHit.getAttribute("data-id") === "0") {
+      tmHit.style.display = "none"
+    }
   }
 }
