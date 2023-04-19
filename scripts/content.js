3e8b5712263b7fea6bad059551bf9062f06401e6
Shun Sakurai <justmyway.yui@gmail.com>
2023-04-19 15:44:23 +0900

    Lighten MT and Public TM hits instead of hiding, using JS again

--- scripts/content.js
+++ scripts/content.js
@@ -6,15 +6,20 @@ observer.observe(document, {
 
 const matchesTab = document.getElementsByClassName('matches');
 const suggestionItems = document.getElementsByClassName('suggestion-item');
+const grayedOutColor = '#bbb';
 
 function onMutation(mutations) {
   for (var i = 0, tab; (tab = matchesTab[i]); i++) {
     tab.classList.add('open');
   }
-}
 
-window.addEventListener('load', function() {
-  const styleSheet = document.styleSheets[0];
-  styleSheet.insertRule('ul.suggestion-item:has(.per-yellow) {display: none;}');
-  styleSheet.insertRule('sugg-target graysmall-message {display: none;}'); // Hide the keyboard shortcut when we hide MT rows
-});
+  for (var j = 0, tmHit; (tmHit = suggestionItems[j]); j++) {
+    var tmDescription = tmHit.children[2].textContent;
+    if (tmDescription.includes('MT') || tmDescription.includes('Public')) {
+      tmHit.children[0].children[0].style.color = grayedOutColor;
+      tmHit.children[1].children[1].style.color = grayedOutColor;
+      tmHit.children[2].children[2].children[0].style.color = grayedOutColor;
+      tmHit.children[2].children[3].children[0].style.color = grayedOutColor;
+    }
+  }
+}
