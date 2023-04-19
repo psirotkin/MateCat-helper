52af6d859157fe31703b82d6950a0de10b1d6316
Shun Sakurai <justmyway.yui@gmail.com>
2023-04-19 19:28:05 +0900

    Add keyboard shortcuts to click OK and Cancel buttons

--- scripts/content.js
+++ scripts/content.js
@@ -7,6 +7,10 @@ observer.observe(document, {
 const matchesTab = document.getElementsByClassName('matches');
 const suggestionItems = document.getElementsByClassName('suggestion-item');
 const grayedOutColor = '#bbb';
+const primaryButtons = document.getElementsByClassName('ui primary button right floated');
+const secondaryButtons = document.getElementsByClassName('ui button cancel-button');
+const okButtons = document.getElementsByClassName('btn-ok');
+const cancelButtons = document.getElementsByClassName('btn-cancel');
 
 function onMutation(mutations) {
   for (var i = 0, tab; (tab = matchesTab[i]); i++) {
@@ -22,4 +26,22 @@ function onMutation(mutations) {
       tmHit.children[2].children[3].children[0].style.color = grayedOutColor;
     }
   }
+
+  [[primaryButtons, 'Y'], [secondaryButtons, 'N'], [okButtons, 'Y'], [cancelButtons, 'N']].forEach(buttonKeyPair => {
+    if (buttonKeyPair[0].length && !buttonKeyPair[0][0].textContent.endsWith(` (Alt+${buttonKeyPair[1]})`)) {
+      buttonKeyPair[0][0].textContent += ` (Alt+${buttonKeyPair[1]})`;
+    }
+  });
 }
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.command == 'yes') {
+    if (!primaryButtons.length && !okButtons.length) return;
+    if (primaryButtons.length) primaryButtons[0].click();
+    if (okButtons.length) okButtons[0].click();
+  } else if (request.command == 'no') {
+    if (!secondaryButtons.length && !cancelButtons.length) return;
+    if (secondaryButtons.length) secondaryButtons[0].click();
+    if (cancelButtons.length) cancelButtons[0].click();
+  }
+});
